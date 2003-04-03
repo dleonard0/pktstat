5a2d0b07ace54b82c7a400e156ab4c84ae8685b1
David Leonard <d+git@adaptive-enterprises.com>
2003-04-03 03:06:58 +0000

    add SUP decoder

--- tcp.h
+++ tcp.h
@@ -3,4 +3,4 @@
 
 void tcp_x11(struct flow *f, const char *data, const char *end);
 void tcp_http(struct flow *f, const char *data, const char *end);
-
+void tcp_sup(struct flow *f, const char *data, const char *end, int isclient);
