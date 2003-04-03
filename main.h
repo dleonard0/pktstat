54d0b2c80c44a4387b9c6235bc4e7cbe70c25602
David Leonard <d+git@adaptive-enterprises.com>
2003-04-03 03:06:14 +0000

    document the flags

--- main.h
+++ main.h
@@ -1,17 +1,17 @@
 /* David Leonard, 2002. Public domain. */
 /* $Id$ */
 
-extern int Bflag;
-extern int cflag;
-extern int Eflag;
-extern int Fflag;
-extern int kflag;
-extern int lflag;
-extern int nflag;
-extern int pflag;
-extern int Pflag;
-extern int tflag;
-extern int Tflag;
-extern int wflag;
+extern int Bflag;	/* Show bytes instead of bits */
+extern int cflag;	/* Keep pkt classes separate (no-combine) */
+extern int Eflag;	/* Ignore errors from pcap (not documented) */
+extern int Fflag;	/* Show full hostnames (not just first part of FQDN) */
+extern int kflag;	/* 'Keep' time in seconds. */
+extern int lflag;	/* last mode; keeps seen classes on screen */
+extern int nflag;	/* no DNS lookups */
+extern int pflag;	/* show packet count instead of bit/byte count */
+extern int Pflag;	/* Disables promiscuous mode */
+extern int tflag;	/* 'top' mode; sort display by bandwidth/packet count */
+extern int Tflag;	/* Show totals */
+extern int wflag;	/* wait time between refresh in seconds */
 
 extern char version[];
