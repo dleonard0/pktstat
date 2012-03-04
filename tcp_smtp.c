47234aa4bf06742651f9ad994243e7d42d620b4f
David Leonard <d+git@adaptive-enterprises.com>
2012-03-04 13:53:00 +0000

    Detect STARTTLS in SMTP so we don't print garbage

--- tcp_smtp.c
+++ tcp_smtp.c
@@ -38,7 +38,7 @@ struct smtp_state {
 	int in_header;		    /* true in DATA's headers */
 	char line[2048];	    /* buffer for data sent to server */
 	int line_len;		    /* length of line read so far */
-	enum { sLINE, sEXPECT_LF } state;
+	enum { sLINE, sEXPECT_LF, sENCRYPTED } state;
 	char to_addr[512];	    /* RCPT TO: */
 	char from_addr[512];	    /* MAIL FROM: */
 };
@@ -62,26 +62,30 @@ normalize_line(line)
 	char *line;
 {
 	char *s, *p;
+	int has_colon;
 
 	s = p = line;
 	/* Skip leading whitespace */
 	while (*s == ' ' || *s == '\t')
 	    s++;
-	/* Uppercase and normalize whitespace */
+	/* Collapse whitespace */
 	while (*s && *s != ':') {
 	    if (*s == ' ' || *s == '\t') {
 		while (*s == ' ' || *s == '\t')
 		    s++;
 		*p++ = ' ';
 	    } else {
-		*p++ = toupper(*s); s++;
+		*p++ = *s++;
 	    }
 	}
 	/* Remove whitespace before a colon */
 	if (*s == ':' && p > line && p[-1] == ' ')
 	    p--;
-	if (*s == ':')
+	if (*s == ':') {
 	    *p++ = *s++;
+	    has_colon = 1;
+	} else
+	    has_colon = 0;
 	/* Remove whitespace after colon */
 	while (*s == ' ' || *s == '\t')
 	    s++;
@@ -98,6 +102,43 @@ normalize_line(line)
 	if (p > line && p[-1] == ' ')
 	    p--;
 	*p = 0;
+
+	/* Uppercase the first word, unless there is a colon in which case
+	 * we uppercase to the colon */
+	for (s = line; *s && *s != (has_colon ? ':' : ' '); s++)
+	    if (*s != ' ')
+		*s = toupper(*s);
+}
+
+/* Normalize an email address
+ *   u@h    -> u@h
+ *   N<u@h> -> u@h
+ */
+
+static void
+normalize_addr(line)
+	char *line;
+{
+	char *s, *t, *u;
+
+	/* Search for the opening angle bracket */
+	for (s = line; *s; s++)
+	    if (*s == '<')
+		break;
+	if (!*s)
+	    return; /* no '<' */
+	s++;	    /* skip '<' */
+
+	/* Check that there is a closing bracket */
+	for (u = s; *u; u++)
+	    if (*u == '>')
+		break;
+	if (*u != '>')
+	    return; /* no '>' */
+
+	for (t = line; s < u; t++,s++)
+	    *t = *s;
+	*t = 0;
 }
 
 static void
@@ -107,8 +148,13 @@ smtp_line(f, line)
 {
 	struct smtp_state *state;
 	const char *s;
+	int addr_changed = 0;
+FILE*log;
 
 	state = (struct smtp_state *)f->udata;
+
+if ((log = fopen("/tmp/smtp.log", "a")))
+   fprintf(log, "smpt_line [%s]\n", line);
 
 	if (state->in_data) {
 	    if (strcmp(line, ".") == 0) {
@@ -134,21 +180,43 @@ smtp_line(f, line)
 	} else {
 	    /* Normalize the command line */
 	    normalize_line(line);
-	    if ((s = strip_prefix(line, "MAIL FROM:")))
+if (log)fprintf(log, "normalized to [%s]\n", line);
+	    if ((s = strip_prefix(line, "MAIL FROM:"))) {
 		snprintf(state->from_addr, sizeof state->from_addr, "%s", s);
-	    else if ((s = strip_prefix(line, "RCPT TO:")))
+		normalize_addr(state->from_addr);
+		addr_changed = 1;
+if (log)fprintf(log, "from_addr = [%s]\n", state->from_addr);
+	    }
+	    else if ((s = strip_prefix(line, "RCPT TO:"))) {
 		snprintf(state->to_addr, sizeof state->to_addr, "%s", s);
+		normalize_addr(state->to_addr);
+		addr_changed = 1;
+if (log)fprintf(log, "to_addr = [%s]\n", state->to_addr);
+	    }
 	    else if (strcmp(line, "DATA") == 0) {
 		state->in_data = 1;
 		state->in_header = 1;
 	    }
+	    else if (strcmp(line, "STARTTLS") == 0) {
+		state->state = sENCRYPTED;
+		snprintf(f->desc, sizeof f->desc, "STARTTLS");
+	    }
 
-	    if (s && *state->from_addr && *state->to_addr) {
+	    if ((addr_changed || 
+	         strcmp(line, "QUIT") == 0 ||
+		 strcmp(line, "DATA") == 0) && 
+		(*state->from_addr && *state->to_addr)) 
+	    {
 		snprintf(f->desc, sizeof f->desc, "%s -> %s", 
 		    state->from_addr, state->to_addr);
-	    } else
-		snprintf(f->desc, sizeof f->desc, "%s", line);
+	    } else {
+		if (strcmp(line, "QUIT") != 0)
+		    snprintf(f->desc, sizeof f->desc, "%s", line);
+	    }
+if (log)fprintf(log, "set desc to: [%s]\n", f->desc);
 	}
+
+if (log)fclose(log);
 }
 
 /*
@@ -168,6 +236,7 @@ tcp_smtp(f, data, end, toserver)
 	    return;
 
 	if (!f->udata) {
+	    /* Initialize state */
 	    state = (struct smtp_state *)malloc(sizeof *state);
 	    if (!state)
 		errx(1, "malloc");
@@ -181,6 +250,10 @@ tcp_smtp(f, data, end, toserver)
 	} else
 	    state = (struct smtp_state *)f->udata;
 
+	if (state->state == sENCRYPTED)
+	    return;	/* Don't bother for STARTTLS */
+
+	/* Extract data, line by line */
 	for (d = data; d < end; d++) 
 	    switch (state->state) {
 	    case sLINE:
